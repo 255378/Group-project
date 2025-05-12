9ae20ca1291cc94917d320f1667d77e5db3bb944
student <student@p.lodz.pl>
2025-05-12 13:58:01 +0200

    operacja dodawania

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void usage(char *program_name) {
+    printf("Uzycie: %s <liczba1> <operacja> <liczba2>\n", program_name);
+    printf("Dostepne operacje: + - / m /\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        usage(argv[0]);
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[3]);
+    char op = argv[2][0];
+    int result;
+
+    switch (op) {
+        case '+':
+            result = a + b;
+            printf("%d + %d = %d\n", a, b, result);
+            break;
+        default:
+            usage(argv[0]);
+    }
+
+    return 0;
+}
